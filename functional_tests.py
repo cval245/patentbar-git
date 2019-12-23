fa0aaeecc2f7d9e5a68ebafd160f2cafeab476b5
Chris <c.val@tutanota.com>
2019-12-22 19:31:14 -0600

    Switch to using unittest for ts, extend with user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,21 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000')
-assert 'index.html' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_accessing_quiz_list(self):
+        # Bob has heard about quiz app
+        # He goest to its homepage
+        self.browser.get('http://localhost:8000/quiz')
+
+        # He notices the Browser heading is patentbar
+        self.assertIn('Patent-Bar', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
